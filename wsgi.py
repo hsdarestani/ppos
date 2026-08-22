bcad37a2c7d3f7579eef6804d47b806ab115d9e1
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-22 20:48:21 +0330

    Register universal campaign engine

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 from app import app, DB_PATH
 from capture import register_capture
 from exports import register_exports
+from campaigns import register_campaigns
 
 register_capture(app, DB_PATH)
 register_exports(app, DB_PATH)
+register_campaigns(app, DB_PATH)
